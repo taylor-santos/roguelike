7d1b862094f86037a5b2e5c1ee176a2f7f58ea17
Taylor Santos <taylor.p.santos@gmail.com>
2021-12-21 20:59:32 -0800

    Fix test failures

--- test/plugin.cpp
+++ test/plugin.cpp
@@ -49,98 +49,6 @@ TEST_CASE("PluginFunction") {
     CHECK_MESSAGE(val == 1, "plugin function should modify argument to point to value 1");
 }
 
-TEST_CASE("PluginReloading") {
-    auto tmp_name     = Plugin::shared_lib_name("test_plugin");
-    auto path         = std::filesystem::current_path() / "plugins";
-    auto tmp_path     = std::filesystem::temp_directory_path();
-    auto tmp_filename = tmp_path / tmp_name;
-    {
-        auto lib_name = Plugin::shared_lib_name("plugin");
-        auto src      = std::ifstream(path / lib_name, std::ios::binary);
-        REQUIRE_MESSAGE(src, "couldn't open plugin file");
-        auto dst = std::ofstream(tmp_filename, std::ios::binary | std::ios::trunc);
-        REQUIRE_MESSAGE(dst, "couldn't open temporary output file");
-        dst << src.rdbuf();
-    }
-    last_write_time(tmp_filename, last_write_time(tmp_filename) - std::chrono::minutes(1));
-    Plugin plugin("test_plugin", tmp_path);
-    auto   start = plugin.get_function("start");
-    int    val   = 0;
-    CHECK_MESSAGE(start(&val) == 0, "plugin function should exist and should return 0");
-    CHECK_MESSAGE(val == 1, "plugin function should modify argument to point to value 1");
-    {
-        auto lib_name = Plugin::shared_lib_name("plugin_patch");
-        auto src      = std::ifstream(path / lib_name, std::ios::binary);
-        REQUIRE_MESSAGE(src, "couldn't open plugin file");
-        auto dst = std::ofstream(tmp_filename, std::ios::binary | std::ios::trunc);
-        REQUIRE_MESSAGE(dst, "couldn't open temporary output file");
-        dst << src.rdbuf();
-    }
-    CHECK_MESSAGE(
-        plugin.reload_if_updated(),
-        "plugin library file has changed, it should detect and load an update");
-    CHECK_MESSAGE(start(&val) == 0, "plugin function should exist and should return 0");
-    CHECK_MESSAGE(val == 2, "updated plugin function should modify argument to point to value 2");
-}
-
-TEST_CASE("PluginRemovedFunction") {
-    auto tmp_name     = Plugin::shared_lib_name("test_plugin");
-    auto path         = std::filesystem::current_path() / "plugins";
-    auto tmp_path     = std::filesystem::temp_directory_path();
-    auto tmp_filename = tmp_path / tmp_name;
-    {
-        auto lib_name = Plugin::shared_lib_name("plugin");
-        auto src      = std::ifstream(path / lib_name, std::ios::binary);
-        REQUIRE_MESSAGE(src, "couldn't open plugin file");
-        auto dst = std::ofstream(tmp_filename, std::ios::binary | std::ios::trunc);
-        REQUIRE_MESSAGE(dst, "couldn't open temporary output file");
-        dst << src.rdbuf();
-    }
-    last_write_time(tmp_filename, last_write_time(tmp_filename) - std::chrono::minutes(1));
-    Plugin plugin("test_plugin", tmp_path);
-    auto   update = plugin.get_function("update");
-    CHECK_MESSAGE(update(nullptr) == 1, "plugin function should exist and should return 1");
-    {
-        auto lib_name = Plugin::shared_lib_name("plugin_patch");
-        auto src      = std::ifstream(path / lib_name, std::ios::binary);
-        REQUIRE_MESSAGE(src, "couldn't open plugin file");
-        auto dst = std::ofstream(tmp_filename, std::ios::binary | std::ios::trunc);
-        REQUIRE_MESSAGE(dst, "couldn't open temporary output file");
-        dst << src.rdbuf();
-    }
-    CHECK_MESSAGE(
-        plugin.reload_if_updated(),
-        "plugin library file has changed, it should detect and load an update");
-    CHECK_MESSAGE(
-        update(nullptr) == -1,
-        "plugin function should no longer exist, so it returns -1");
-    CHECK_MESSAGE(update.is_valid() == false, "plugin function should now be invalidated");
-}
-
-TEST_CASE("PluginReloadingDeleted") {
-    auto tmp_name     = Plugin::shared_lib_name("test_plugin");
-    auto path         = std::filesystem::current_path() / "plugins";
-    auto tmp_path     = std::filesystem::temp_directory_path();
-    auto tmp_filename = tmp_path / tmp_name;
-    {
-        auto lib_name = Plugin::shared_lib_name("plugin");
-        auto src      = std::ifstream(path / lib_name, std::ios::binary);
-        REQUIRE_MESSAGE(src, "couldn't open plugin file");
-        auto dst = std::ofstream(tmp_filename, std::ios::binary | std::ios::trunc);
-        REQUIRE_MESSAGE(dst, "couldn't open temporary output file");
-        dst << src.rdbuf();
-    }
-    last_write_time(tmp_filename, last_write_time(tmp_filename) - std::chrono::minutes(1));
-    Plugin plugin("test_plugin", tmp_path);
-    {
-        auto dst = std::ofstream(tmp_filename, std::ios::binary | std::ios::trunc);
-        REQUIRE_MESSAGE(dst, "couldn't open temporary output file");
-    }
-    CHECK_MESSAGE(
-        plugin.reload_if_updated(100) == false,
-        "original plugin is deleted, reloading should fail after a delay");
-}
-
 TEST_CASE("PluginMoveConstruct") {
     auto   path = std::filesystem::current_path() / "plugins";
     Plugin plugin("plugin", path);
@@ -174,4 +82,99 @@ TEST_CASE("PluginMoveAssign") {
     CHECK_MESSAGE(val == 1, "function should still have the same effect");
     auto update = copy.get_function("update");
     CHECK_MESSAGE(update.is_valid(), "new plugin should be able to access new functions");
+}
+
+TEST_CASE("PluginReloading") {
+    auto tmp_name     = Plugin::shared_lib_name("test_plugin");
+    auto path         = std::filesystem::current_path() / "plugins";
+    auto tmp_path     = std::filesystem::temp_directory_path();
+    auto tmp_filename = tmp_path / tmp_name;
+    {
+        auto lib_name = Plugin::shared_lib_name("plugin");
+        auto src      = std::ifstream(path / lib_name, std::ios::binary);
+        REQUIRE_MESSAGE(src, "couldn't open plugin file");
+        auto dst = std::ofstream(tmp_filename, std::ios::binary | std::ios::trunc);
+        REQUIRE_MESSAGE(dst, "couldn't open temporary output file");
+        dst << src.rdbuf();
+    }
+    using namespace std::chrono_literals;
+    last_write_time(tmp_filename, last_write_time(tmp_filename) - 1min);
+    Plugin plugin("test_plugin", tmp_path);
+    auto   start = plugin.get_function("start");
+    int    val   = 0;
+    CHECK_MESSAGE(start(&val) == 0, "plugin function should exist and should return 0");
+    CHECK_MESSAGE(val == 1, "plugin function should modify argument to point to value 1");
+    {
+        auto lib_name = Plugin::shared_lib_name("plugin_patch");
+        auto src      = std::ifstream(path / lib_name, std::ios::binary);
+        REQUIRE_MESSAGE(src, "couldn't open plugin file");
+        auto dst = std::ofstream(tmp_filename, std::ios::binary | std::ios::trunc);
+        REQUIRE_MESSAGE(dst, "couldn't open temporary output file");
+        dst << src.rdbuf();
+    }
+    CHECK_MESSAGE(
+        plugin.reload_if_updated(),
+        "plugin library file has changed, it should detect and load an update");
+    CHECK_MESSAGE(start(&val) == 0, "plugin function should exist and should return 0");
+    CHECK_MESSAGE(val == 2, "updated plugin function should modify argument to point to value 2");
+}
+
+TEST_CASE("PluginRemovedFunction") {
+    auto tmp_name     = Plugin::shared_lib_name("test_plugin");
+    auto path         = std::filesystem::current_path() / "plugins";
+    auto tmp_path     = std::filesystem::temp_directory_path();
+    auto tmp_filename = tmp_path / tmp_name;
+    {
+        auto lib_name = Plugin::shared_lib_name("plugin");
+        auto src      = std::ifstream(path / lib_name, std::ios::binary);
+        REQUIRE_MESSAGE(src, "couldn't open plugin file");
+        auto dst = std::ofstream(tmp_filename, std::ios::binary | std::ios::trunc);
+        REQUIRE_MESSAGE(dst, "couldn't open temporary output file");
+        dst << src.rdbuf();
+    }
+    using namespace std::chrono_literals;
+    last_write_time(tmp_filename, last_write_time(tmp_filename) - 1min);
+    Plugin plugin("test_plugin", tmp_path);
+    auto   update = plugin.get_function("update");
+    CHECK_MESSAGE(update(nullptr) == 1, "plugin function should exist and should return 1");
+    {
+        auto lib_name = Plugin::shared_lib_name("plugin_patch");
+        auto src      = std::ifstream(path / lib_name, std::ios::binary);
+        REQUIRE_MESSAGE(src, "couldn't open plugin file");
+        auto dst = std::ofstream(tmp_filename, std::ios::binary | std::ios::trunc);
+        REQUIRE_MESSAGE(dst, "couldn't open temporary output file");
+        dst << src.rdbuf();
+    }
+    CHECK_MESSAGE(
+        plugin.reload_if_updated(),
+        "plugin library file has changed, it should detect and load an update");
+    CHECK_MESSAGE(
+        update(nullptr) == -1,
+        "plugin function should no longer exist, so it returns -1");
+    CHECK_MESSAGE(update.is_valid() == false, "plugin function should now be invalidated");
+}
+
+TEST_CASE("PluginReloadingDeleted") {
+    auto tmp_name     = Plugin::shared_lib_name("test_plugin");
+    auto path         = std::filesystem::current_path() / "plugins";
+    auto tmp_path     = std::filesystem::temp_directory_path();
+    auto tmp_filename = tmp_path / tmp_name;
+    {
+        auto lib_name = Plugin::shared_lib_name("plugin");
+        auto src      = std::ifstream(path / lib_name, std::ios::binary);
+        REQUIRE_MESSAGE(src, "couldn't open plugin file");
+        auto dst = std::ofstream(tmp_filename, std::ios::binary | std::ios::trunc);
+        REQUIRE_MESSAGE(dst, "couldn't open temporary output file");
+        dst << src.rdbuf();
+    }
+    using namespace std::chrono_literals;
+    last_write_time(tmp_filename, last_write_time(tmp_filename) - 1min);
+    Plugin plugin("test_plugin", tmp_path);
+    {
+        auto dst = std::ofstream(tmp_filename, std::ios::binary | std::ios::trunc);
+        REQUIRE_MESSAGE(dst, "couldn't open temporary output file");
+    }
+    CHECK_MESSAGE(
+        plugin.reload_if_updated(100) == false,
+        "original plugin is deleted, reloading should fail after a delay");
 }
